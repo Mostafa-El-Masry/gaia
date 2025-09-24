bfbeb3432a558f7ee48adfc7009dabe8f1311ea6
Mostafa-El-Masry <mostafa.abdelfattah2021@gmail.com>
2025-09-24 16:41:58 +0300

    updating props

--- app/components/LogEntry.tsx
+++ app/components/LogEntry.tsx
@@ -8,7 +8,7 @@ interface LogEntryProps {
   content: string;
 }
 
-const LogEntry: React.FC<LogEntryProps> = ({ id, date, title, content }) => {
+const LogEntry = ({ id, date, title, content }: LogEntryProps) => {
   return (
     <div className="mb-4 max-w-3xl mx-auto p-4 border border-gray-200 rounded-lg shadow-sm hover:shadow-md transition">
       <h3 className="text-xl font-bold text-gray-800">{title}</h3>
