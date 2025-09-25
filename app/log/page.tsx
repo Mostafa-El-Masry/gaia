bca70c0988f6cd6bf0f41b9433c86351bf8caa39
Mostafa-El-Masry <mostafa.abdelfattah2021@gmail.com>
2025-09-25 12:54:23 +0300

    adding search bar to log page

--- app/log/page.tsx
+++ app/log/page.tsx
@@ -1,14 +1,33 @@
+"use client";
 // app/log/page.tsx
 
 import { logData } from "@/data/logData";
 import LogEntry from "../components/LogEntry";
-
+import { useState } from "react";
+import { Enriqueta } from "next/font/google";
 
 const LogPage = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredLogs = logData.filter(entry => 
+    entry.title.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase()) ||
+    entry.content.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())
+  )
   return (
     <main className="container max-w-3xl mx-auto p-6">
-      <h1 className="text-3xl font-bold mb-6 text-center">GAIA - Development Log</h1>
-      {logData.map((log) => (
+      <h1 className="text-3xl font-bold mb-6 text-center">
+        GAIA - Development Log
+      </h1>
+      <input
+        type="text"
+        placeholder="Search Logs..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="border p-2 rounded mb-4 w-full"
+      />
+
+      
+      {filteredLogs.map((log) => (
         <LogEntry
           key={log.id}
           title={log.title}
