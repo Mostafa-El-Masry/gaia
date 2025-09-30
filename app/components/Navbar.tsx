1134bdb18ff2cf274940bdd977fbb64e252ac5ef
Mostafa-El-Masry <mostafa.abdelfattah2021@gmail.com>
2025-09-30 07:06:56 +0300

    Adding a Dark/Light Theme

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import ThemeToggle from "./ThemeToggle";
@@ -18,31 +18,31 @@ export default function Navbar() {
     <nav className="bg-gray-900 text-white px-6 py-4 shadow-lg">
       <div className="container mx-auto flex justify-between items-center">
         {/* Left side: Logo */}
-        <div className="text-2xl font-bold tracking-wide">
-          GAIA
-        </div>
+        <div className="text-2xl font-bold tracking-wide">GAIA</div>
 
         {/* Right side: Links */}
         <div className="space-x-6 hidden md:flex">
           {navLinks.map((link) => {
-              const isActive = pathname === link.href;
-              return (
-                  <Link
-                    href={link.href}
-                    className={`hover:text-emerald-400 transition ${isActive ? "underline font-bold cursor:pointer text-emerald-300" : ""}`}
-                    key={link.href}>
-                    {link.name}
-                  </Link>
-              );
-            })}
-            
+            const isActive = pathname === link.href;
+            return (
+              <Link
+                href={link.href}
+                className={`hover:text-emerald-400 transition ${
+                  isActive
+                    ? "underline font-bold cursor:pointer text-emerald-300"
+                    : ""
+                }`}
+                key={link.href}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
         </div>
-<ThemeToggle />
+        <ThemeToggle />
         {/* Mobile Menu Button */}
         <div className="md:hidden">
-          <button className="text-white hover:text-emerald-400">
-            ☰
-          </button>
+          <button className="text-white hover:text-emerald-400">☰</button>
         </div>
       </div>
     </nav>
