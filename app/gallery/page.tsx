0010b22081a150b1e4a98f4d575e7a98f0cded8f
Mostafa-El-Masry <mostafa.abdelfattah2021@gmail.com>
2025-09-30 14:17:45 +0300

    adding a common header

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -2,12 +2,9 @@ import SectionHeader from "../components/SectionHeader";
 
 export default function GalleryPage() {
   return (
-    <section className="p-6 space-y-4">
-      <SectionHeader
-        title="Gallery"
-        description="Your visual library. Store images, artwork, and memories."
-      />
-      <button className="border px-4 py-2 rounded">Add Image</button>
-    </section>
+    <SectionHeader
+  title="Gallery"
+  description="This is the where all images will exist."
+/>
   );
 }
